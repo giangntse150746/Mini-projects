72c89c3e8175f183bdc19eaecb17adb52219b5d1
GiangNT <giangntse150746@gmail.com>
2024-08-16 02:21:58 +0700

    Update template [temporary]

--- cards-hover/main.js
+++ cards-hover/main.js
@@ -1,35 +1,30 @@
-let activeIndex = 0;
-
 const groups = document.getElementsByClassName("card-group");
+let activeIndex = 0;
 
 const handleLoveClick = () => {
   const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
-
   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
     nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
   currentGroup.dataset.status = "after";
-
   nextGroup.dataset.status = "becoming-active-from-before";
 
   setTimeout(() => {
     nextGroup.dataset.status = "active";
     activeIndex = nextIndex;
   });
-}
+};
 
 const handleHateClick = () => {
   const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
-
   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
     nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
   currentGroup.dataset.status = "before";
-
   nextGroup.dataset.status = "becoming-active-from-after";
 
   setTimeout(() => {
     nextGroup.dataset.status = "active";
     activeIndex = nextIndex;
   });
-}
+};
